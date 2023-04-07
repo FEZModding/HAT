38907743aab6a41ce4ce89cb52c1df23110ba79c
Terry Hearst <thearst3rd@gmail.com>
2023-04-07 14:27:42 -0400

    Use updated dependency model from FEZUG

--- Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("HAT")]
-[assembly: AssemblyDescription("Mod loader for FEZ")]
-[assembly: AssemblyCopyright("Copyright © Krzyhau 2023")]
-
-[assembly: ComVisible(false)]
