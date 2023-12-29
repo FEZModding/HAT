93a048efd789ba0f7dfe615038bb8836663e79fc
Krzyhau <krzyht@gmail.com>
2023-12-29 18:46:47 +0100

    encapsulate entire program in try-catch
    resolves #6

---
+++ Patches/Program.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+namespace FezGame
+{
+    internal static class patch_Program
+    {
+        private static extern void orig_Main(string[] args);
+
+        private static void Main(string[] args)
+        {
+            // The game is encapsulating the main game component in a Logger-based try-catch.
+            // However, occasionally, error can occur during HAT initialisation, or when the
+            // game is shutting down. We want to keep track of it.
+
+            Logger.Try(orig_Main, args);
+        }
+    }
+}
