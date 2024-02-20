3fb4de3af56101a1478066e928a6ebb89ddcd74e
Krzyhau <krzyht@gmail.com>
2024-02-20 21:24:45 +0100

    add mod identity extensions

---
+++ Source/ModDefinition/ModIdentityHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace HatModLoader.Source.ModDefinition
+{
+    public static class ModIdentityHelper
+    {
+        public static Mod GetModByGameComponent<T>(this Hat hat) where T : GameComponent
+        {
+            return hat.Mods.Where(mod => mod.Components.Any(component => component is T)).FirstOrDefault();
+        }
+
+        public static Mod GetOwnMod(this GameComponent gameComponent)
+        {
+            return Hat.Instance.Mods.Where(mod => mod.Components.Contains(gameComponent)).FirstOrDefault();
+        }
+    }
+}
