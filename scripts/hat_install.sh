d0529c23ae2002640e92f47e66797a4a55699f42
Krzyhau <contact@krzyhau.pl>
2025-04-27 09:45:49 +0200

    improve linux installation script

--- scripts/hat_install.sh
+++ scripts/hat_install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-export MONOMOD_DEPDIRS="HATDependencies/MonoMod;HATDependencies/FEZRepacker.Core"
-mono "HATDependencies/MonoMod/MonoMod.exe" FEZ.exe
+# Move to script's directory
+cd "`dirname "$0"`"
+
+# Copy all files from HATDependencies for the duration of patching
+temp_files=()
+while IFS= read -r -d '' file; do
+    cp "$file" . && copied_files+=("$(basename "$file")")
+done < <(find HATDependencies -type f -print0)
+
+# Patching
+mono MonoMod.exe FEZ.exe
+
+# Cleanup
+for f in "${copied_files[@]}"; do
+    rm -f -- "$f"
+done
