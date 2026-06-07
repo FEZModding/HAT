382ed2852268ff6f5e31c377a7f40595d4bca822
Chris Hausky <contact@krzyhau.pl>
2026-06-08 01:11:55 +0200

    fix: move custom save data to less invasive place

--- Plugin/Source/Storage/CustomSaveDataUtil.cs
+++ Plugin/Source/Storage/CustomSaveDataUtil.cs
@@ -2,12 +2,11 @@
 
 namespace HatModLoader.Source.Storage;
 
-// HAT is storing dummy level data with its ScriptingState as custom data container.
-// Almost nothing in the game is iterating over the level data, so this should be safe operation.
+// HAT is storing custom save data in what seems to be unused list of strings "EarnedGamerPictures".
+// If it turns out to be used on any platform (presumably Xbox?), we should write a patch to prevent
+// our custom data from colliding with any logic (if even).
 public static class CustomSaveDataUtil
 {
-    private static string CustomDataPrefix = "$HAT$_";
-
     public static string GetCustomData(this SaveData saveData, string key, string defaultValue)
     {
         return saveData.TryGetCustomData(key, out var value) ? value : defaultValue;
@@ -15,9 +14,12 @@ public static class CustomSaveDataUtil
 
     public static bool TryGetCustomData(this SaveData saveData, string key, out string value)
     {
-        if (saveData.World.TryGetValue($"{CustomDataPrefix}{key}", out var levelSaveData))
+        var dataPrefix = GetCustomDataPrefix(key);
+        var existingRecord = saveData.EarnedGamerPictures.FindIndex(text => text.StartsWith(dataPrefix));
+        
+        if (existingRecord >= 0)
         {
-            value = levelSaveData.ScriptingState;
+            value = saveData.EarnedGamerPictures[existingRecord].Substring(dataPrefix.Length);
             return true;
         }
         value = null;
@@ -26,11 +28,22 @@ public static class CustomSaveDataUtil
 
     public static void SetCustomData(this SaveData saveData, string key, string value)
     {
-        if (!saveData.World.TryGetValue($"{CustomDataPrefix}{key}", out var levelSaveData))
+        var dataPrefix = GetCustomDataPrefix(key);
+        var newValue = dataPrefix + value;
+        var existingRecord = saveData.EarnedGamerPictures.FindIndex(text => text.StartsWith(dataPrefix));
+        
+        if (existingRecord >= 0)
         {
-            levelSaveData = new LevelSaveData();
-            saveData.World.Add($"{CustomDataPrefix}{key}", levelSaveData);
+            saveData.EarnedGamerPictures[existingRecord] = newValue;
         }
-        levelSaveData.ScriptingState = value;
+        else
+        {
+            saveData.EarnedGamerPictures.Add(newValue);
+        }
+    }
+
+    private static string GetCustomDataPrefix(string key)
+    {
+        return $"{key}:";
     }
 }
