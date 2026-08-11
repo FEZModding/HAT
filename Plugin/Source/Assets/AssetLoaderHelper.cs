8902dfae99fb6c2f6fd57884ff6e88a586fbf007
Chris Hausky <contact@krzyhau.pl>
2026-08-11 21:26:27 +0200

    fix: show full error message on asset error

--- Plugin/Source/Assets/AssetLoaderHelper.cs
+++ Plugin/Source/Assets/AssetLoaderHelper.cs
@@ -48,7 +48,7 @@ namespace HatModLoader.Source.Assets
 
                     if (!savedAnyRawFiles)
                     {
-                        Logger.Log("HAT", $"Could not convert asset bundle {bundle.BundlePath}: {ex.Message}\n{ex.StackTrace}");
+                        Logger.Log("HAT", $"Could not convert asset bundle {bundle.BundlePath}: {ex}");
                     }
                 }
 
